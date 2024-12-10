814a5c44baff917e2e1508524054f09c1bebda8a
BigShuff <121031780+BigShuff@users.noreply.github.com>
2024-12-10 12:13:10 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const hostname = "127.0.0.1";
+const port = 3002;
+
+// express app
+const app = express();
+
+// register view engine
+app.set('view engine', 'ejs');
+
+//this tells the app to use public for all static files I.e images and css etc
+app.use(express.static('public'));
+
+
+// listen for requests
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+
+  app.get('/', (req, res) => {
+    // const blogs = [
+    //   {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    //   {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    //   {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    // ];
+  
+  //note the dynamic title variable for each page
+    //res.render('index', { title: 'Home', blogs });
+    res.render('index', { title: 'Home' });
+  });
+
+  app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+  });
+
+  app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+  });
+  
