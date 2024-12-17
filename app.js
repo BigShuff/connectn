83cd6f09f412e5a1c40e10c6ab4995188227a89e
BigShuff <121031780+BigShuff@users.noreply.github.com>
2024-12-17 19:40:05 +0000

    dec 17

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const path= require('path');
 const bcrypt = require('bcrypt');
 const hostname = "127.0.0.1";
 const port = 3002;
+const regInfo = require('./js/configdb');
 
 // express app
 const app = express();
 
 // register view engine
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }))
 
 //this tells the app to use public for all static files I.e images and css etc
 app.use(express.static('public'));
@@ -41,6 +43,45 @@ app.listen(port, hostname, () => {
 
   app.get('/login', (req, res) => {
     res.render('login', {title: 'Login'})
+  })
+
+  app.get('/signup', (req, res) => {
+    res.render('signup', {title: 'Sign Up'})
+  })
+
+  app.post('/signup', async (req, res) => {
+   const formattedDate = new Date();
+   const registrationDate = formattedDate.toLocaleString('en-GB');
+   const aReg = new regInfo({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+      date: registrationDate
+    })
+    const existingUser = await regInfo.findOne({username: aReg.username})
+    if (existingUser) {
+      res.send('User already exists. Choose a different user name');
+    } else {
+      //hash the password
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(aReg.password, saltRounds);
+      aReg.password = hashedPassword;
+      aReg.save()
+    .then(result => {
+      res.redirect('/')
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    }
+    
+
+    // const userData = await regInfo.insertMany(aReg)
+    // console.log(userData)
+    // console.log(req.body.username);
+    // console.log(req.body.email);
+    // console.log(req.body.password);
+    // console.log(registrationDate);
   })
 
   app.use((req, res) => {
